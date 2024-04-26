a8aaf1b026c57682baa002ba8b82473a1c4f451f
Calin <chelcea.calin@yahoo.com>
2024-04-26 22:40:12 +0300

    added filtering

--- src/main/java/com/agendaCorespondenta/calin/Agenda/Corespondenta/repository/ContactRepository.java
+++ src/main/java/com/agendaCorespondenta/calin/Agenda/Corespondenta/repository/ContactRepository.java
@@ -1,7 +1,6 @@
 package com.agendaCorespondenta.calin.Agenda.Corespondenta.repository;
 
 import com.agendaCorespondenta.calin.Agenda.Corespondenta.model.Contact;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
